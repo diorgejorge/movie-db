22f0cad6405b5550a4deab97be84447e53421ccb
diorgejorge <diorge@gmail.com>
2019-05-23 19:08:32 -0300

    Update moviedb.js

--- src/services/moviedb.js
+++ src/services/moviedb.js
@@ -4,6 +4,6 @@ import moment from 'moment/moment';
 import { REACT_APP_THEMOVIEDB_KEY } from 'react-native-dotenv'
 
 const moviesdb = axios.create({
-    baseURL: `https://api.themoviedb.org/3/discover/movie?api_key=${REACT_APP_THEMOVIEDB_KEY}&language=pt-BR&with_genres=10751,16&sort_by=release_date.desc&include_adult=false&include_video=false&primary_release_date.gte=${moment(new Date()).format('YYYY-MM-DD')}&primary_release_date.lte=${moment(new Date()).add(7,'days').format('YYYY-MM-DD')}`
+    baseURL: `https://api.themoviedb.org/3/discover/movie?api_key=${REACT_APP_THEMOVIEDB_KEY}&region=BR&language=pt-BR&with_genres=10751,16&sort_by=release_date.desc&include_adult=false&include_video=false&primary_release_date.gte=${moment(new Date()).format('YYYY-MM-DD')}&primary_release_date.lte=${moment(new Date()).add(7,'days').format('YYYY-MM-DD')}`
 });
 export default moviesdb;
